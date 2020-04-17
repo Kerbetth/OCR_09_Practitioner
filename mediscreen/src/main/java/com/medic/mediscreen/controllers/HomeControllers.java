7511d02daa42743a306d9ff9dce65fc5b4e5806d
Kerbetth <57868057+Kerbetth@users.noreply.github.com>
2020-04-17 18:25:19 +0200

    premiere structure

---
+++ mediscreen/src/main/java/com/medic/mediscreen/controllers/HomeControllers.java
@@ -0,0 +1,33 @@
+package com.medic.mediscreen.controllers;
+
+
+import com.paymybuddy.transferapps.service.ConnectionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * -the root of the url give link to login or create an account
+ * -userHome url is the main page of connected users
+ */
+
+@Controller
+public class HomeControllers {
+
+    @Autowired
+    private ConnectionService connectionService;
+
+    @RequestMapping("/")
+    public String getLog(Model model) {
+        return "LogPage";
+    }
+
+    @GetMapping("/userHome")
+    public String userPage(Model model) {
+            model.addAttribute("userAccount", connectionService.getAccountInfo());
+            return "UserPage";
+    }
+
+}
---
+++ mediscreen/src/main/java/com/medic/mediscreen/domain/Patient.java
@@ -0,0 +1,12 @@
+package com.medic.mediscreen.domain;
+
+import java.util.Date;
+
+public class Patient {
+    String family;
+    String given;
+    Date dob;
+    char sex;
+    String address;
+    String phone;
+}
